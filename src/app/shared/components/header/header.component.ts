6eb4ee3e925229cf8b01755453b8e6e0caf1215e
rgdev12 <83981427+rgdev12@users.noreply.github.com>
2026-02-06 09:43:23 -0600

    💄Implementing logos in the login and dashboard and using PrimeNG's popover component in the header.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { LucideAngularModule, CircleUserRound , Lock, FileText, BarChart3, LogOut } from 'lucide-angular';
 import { AuthService } from '@core/services/auth.service';
+import { Popover } from 'primeng/popover';
 
 @Component({
   selector: 'app-header',
-  imports: [LucideAngularModule, RouterLink],
+  imports: [LucideAngularModule, RouterLink, Popover],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -21,20 +22,7 @@ export class Header {
   BarChartIcon = BarChart3;
   LogOutIcon = LogOut;
 
-  // Menu state
-  isMenuOpen = false;
-
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  closeMenu(): void {
-    this.isMenuOpen = false;
-  }
-
   async logout(): Promise<void> {
-    this.closeMenu();
     await this.authService.signOut();
-    // this.router.navigate(['/auth/login']);
   }
 }
